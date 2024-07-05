18328ce1c2c16f670f18e5c403719f236e72d815
Léandre <>
2024-07-06 00:48:48 +0200

    feat: adds pin and init config file

--- Baio_PCA_firmware/Baio_PCA_firmware.ino
+++ Baio_PCA_firmware/Baio_PCA_firmware.ino
@@ -3,19 +3,8 @@
 #define DCSBIOS_DEFAULT_SERIAL
 #include "DcsBios.h"
 
-/********* PINs SETUP **********/
-const int PCA_SDA = 2;
-const int PCA_SCL = 3;
-const int PCAlatchPin = 7;
-const int PCAdataPin = 4;
-const int PCAclockPin = 6;
-const byte PCAbacklightPin = 5;
+#include "PinsAndInitConfig.h"
 
-
-
-/********* INIT VALUES AND CST **********/
-byte PCAvalues[2] = {255, 255};
-const byte registreLength = 8;
 
 
 Joystick_ Joystick(
---
+++ Baio_PCA_firmware/PinsAndInitConfig.h
@@ -0,0 +1,12 @@
+/********* PINS **********/
+const int PCA_SDA = 2;
+const int PCA_SCL = 3;
+const int PCAlatchPin = 7;
+const int PCAdataPin = 4;
+const int PCAclockPin = 6;
+const byte PCAbacklightPin = 5;
+
+
+/********* INIT VALUES AND CST **********/
+byte PCAvalues[2] = {255, 255};
+const byte registreLength = 8;
